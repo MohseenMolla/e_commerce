9d1b9b539468bb7a2fe2725809fd1f15bab81f26
Mohseen Molla <mdmohseen800@gmail.com>
2026-02-07 21:18:28 +0600

    Update API base URL to production environment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://e-commerce2-ihrp.onrender.com/api",
 });
 
 // Routes that DO NOT need token
